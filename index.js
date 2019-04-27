42d9e2b3dd0ca5df60c31c86cf4015eb16e73691
Madeline Carr <madelinencarr@Gmail.com>
2019-04-27 09:22:20 -0400

    Mobile Navigation

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-$(document).ready(function () {
-    $(document).on("scroll", onScroll);
+$(document).ready(function() {
+  $(document).on("scroll", onScroll);
 
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
+  $('a[href^="#"]').on("click", function(e) {
+    e.preventDefault();
+    $(document).off("scroll");
 
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-
-        var target = this.hash;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 600, function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
+    $("a").each(function() {
+      $(this).removeClass("active");
     });
+    $(this).addClass("active");
+
+    var target = this.hash;
+    $target = $(target);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top
+        },
+        600,
+        function() {
+          window.location.hash = target;
+          $(document).on("scroll", onScroll);
+        }
+      );
+  });
 });
 
 function onScroll(event) {
-    var scrollPosition = $(document).scrollTop();
-    $('nav a').each(function () {
-        var currentLink = $(this);
-        var refElement = $(currentLink.attr("href"));
-        if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-            $('nav ul li a').removeClass("active");
-            currentLink.addClass("active");
-        }
-        else {
-            currentLink.removeClass("active");
-        }
-    });
+  var scrollPosition = $(document).scrollTop();
+  $("nav a").each(function() {
+    var currentLink = $(this);
+    var refElement = $(currentLink.attr("href"));
+    if (
+      refElement.position().top <= scrollPosition &&
+      refElement.position().top + refElement.height() > scrollPosition
+    ) {
+      $("nav ul li a").removeClass("active");
+      currentLink.addClass("active");
+    } else {
+      currentLink.removeClass("active");
+    }
+  });
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
